2031dcbde10f28e1ecca77841513588432be4599
Tomich0012 <tomheyerick1@gmail.com>
2025-03-24 13:57:17 +0100

    random def

--- folder_to_analyse/real_estate_scraper/pipelines.py
+++ folder_to_analyse/real_estate_scraper/pipelines.py
@@ -6,8 +6,25 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import random
 
 class RealEstateScraperPipeline:
     def process_item(self, item, spider):
         return item
+
+        
+
+class RandomStuff:
+    def do_random_stuff(self):
+        return random.choice(['apple', 'banana', 'cherry', 'date', 'elderberry'])
+
+def do_random_stuff():
+    """Return a random fruit from a predefined list."""
+    return random.choice(['apple', 'banana', 'cherry', 'date', 'elderberry'])
+
+
+
+
+random_stuff_instance = RandomStuff()
+random_item = random_stuff_instance.do_random_stuff()
+print(random_item)
